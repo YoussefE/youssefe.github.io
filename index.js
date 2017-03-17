37df93408fba595c4980183e5bc1529da1460cc7
Youssef El-Hasbani <Youssef El-Hasbani>
2017-03-17 08:06:25 -0400

    Preloading images

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-var images = ["url(images/ParliamentHill.jpg)", "url(images/CorktownBridge.jpg)", "url(images/NationalGallery.jpg)"];
+var images = ["images/ParliamentHill.jpg", "images/CorktownBridge.jpg", "images/NationalGallery.jpg"];
 var itr = 0
+
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+        $('<img/>')[0].src = this;
+        // Alternatively you could use:
+        // (new Image()).src = this;
+    });
+}
+
+preload(images);
 
 $(document).ready(function(){
   setInterval(function(){
     itr = (itr + 1) % 3;
     $(".slide").fadeOut(1500, function(){
-      $(".slide").css({"background": images[itr], "background-size": "cover", "background-position": "center"})
+      $(".slide").css({"background": "url(" + images[itr] + ")", "background-size": "cover", "background-position": "center"})
       $(".slide").fadeIn(1500);
     });
   }, 6000);
